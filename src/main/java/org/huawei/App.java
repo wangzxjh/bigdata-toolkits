9818ee1a4e24f748ca401e82ab6cd54cba31e3c1
wangzhen <891562708@qq.com>
2021-07-10 11:10:31 +0800

    first commit

---
+++ src/main/java/org/huawei/App.java
@@ -0,0 +1,13 @@
+package org.huawei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
